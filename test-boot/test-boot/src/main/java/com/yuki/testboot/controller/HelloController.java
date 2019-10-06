142aac7fe6d7750861091da8324e791d98254c5b
Shaodachuan <577746040@qq.com>
2019-10-06 23:49:48 +0800

    添加Spring Boot 创建代码

---
+++ test-boot/test-boot/src/main/java/com/yuki/testboot/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.yuki.testboot.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("hello")
+    public String hello(){
+        return "hello-spring-boot";
+    }
+
+}
