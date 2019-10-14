b6d3703489dd1e6e066d9b9ec10d3810b4adc29e
Shaodachuan <577746040@qq.com>
2019-10-15 04:06:20 +0800

    添加Spring Boot AOP代码

---
+++ test-thread/src/com/yuki/see/SeeDemo.java
@@ -0,0 +1,22 @@
+package com.yuki.see;
+
+public class SeeDemo {
+
+    private static boolean shutdown = false;
+
+    static class HelloThread extends Thread {
+        @Override
+        public void run() {
+            while(!shutdown){
+                // do nothing
+            }
+            System.out.println("exit hello");
+        }
+    }
+    public static void main(String[] args) throws InterruptedException {
+        new HelloThread().start();
+        Thread.sleep(1000);
+        shutdown = true;
+        System.out.println("exit main");
+    }
+}
