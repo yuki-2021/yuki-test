e7fc4a6ef87b47ab43d9f1e9b19d3dbc3cd1f9ad
Shaodachuan <577746040@qq.com>
2019-11-11 21:38:46 +0800

    log4j2详解

---
+++ bx-boot/log4j2-demo/src/main/java/Yuki4j.java
@@ -0,0 +1,15 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Yuki4j {
+
+    public static void main(String[] args) {
+        Logger logger = LogManager.getLogger(LogManager.ROOT_LOGGER_NAME);
+        logger.trace("trace level");
+        logger.debug("debug level");
+        logger.info("info level");
+        logger.warn("warn level");
+        logger.error("error level");
+        logger.fatal("fatal level");
+    }
+}
